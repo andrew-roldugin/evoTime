bd87d9ff2a74b1a5d35a23711e2179e582bc6a5c
andrew-roldugin <russianracer201507@gmail.com>
2022-11-02 11:44:49 +0300

    Инициализация клиентской части

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {FormattedDate, IntlProvider} from "react-intl";
+import axios from "axios";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [date, setDate] = useState('');
+
+    useEffect(() => {
+        axios('http://localhost:8081/api/test/test-db', {
+            method: "get",
+            headers: {
+                'Content-Type': 'text/plain',
+            },
+            auth: {
+                password: "1234",
+                "username": "test_user"
+            }
+        })
+            .then(res => setDate(res.data))
+    }, [])
+
+    return (
+        <IntlProvider messages={{}} locale='ru' defaultLocale='ru'>
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+
+                    {date.length > 0
+                        ? <p>Соединение с сервером установлено <FormattedDate value={date}
+                                                                              dateStyle={"long"}></FormattedDate></p>
+                        : <p>Подключение...</p>}
+                    <a
+                        className="App-link"
+                        href="https://ru.reactjs.org/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Официальная документация React
+                    </a>
+                </header>
+            </div>
+        </IntlProvider>
+
+    );
 }
 
 export default App;
